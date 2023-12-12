cce787d0caa611879556be0ad2a4c980d5a02975
tuananh3110 <tuananhitsg@gmail.com>
2023-12-12 23:08:34 +0700

    finish create list action

---
+++ app/(platform)/(dashboard)/board/[boardId]/_component/list-container.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { ListWithCard } from '@/type'
+import { ListForm } from './list-form'
+
+interface ListContainerProps {
+  data: ListWithCard[]
+  boardId: string
+}
+
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  return (
+    <ol>
+      <ListForm />
+      <div className="flex-shrink-0 w-1" />
+    </ol>
+  )
+}
